6dfa36e4b6127d4e613b2cbcb9f4239c9881196f
Santiago Farías <49741799+santiagofh@users.noreply.github.com>
2024-06-11 12:41:42 -0400

    Update 04_Indice_Prioridad_Social.py

--- pages/04_Indice_Prioridad_Social.py
+++ pages/04_Indice_Prioridad_Social.py
@@ -6,25 +6,13 @@ import plotly.express as px
 # Configuración de la página
 st.set_page_config(page_title="Análisis de Comunas en Región Metropolitana: IPS", layout='wide', initial_sidebar_state='expanded')
 #%%
-# # URL del PDF
-# pdf_url = "data_raw/ÍNDICE DE PRIORIDAD SOCIAL 2022.pdf"
 
-# def display_pdf(file_path):
-#     with open(file_path, "rb") as file:
-#         st.download_button(
-#             label="Descargar Índice de Prioridad Social 2022",
-#             data=file,
-#             file_name="ÍNDICE DE PRIORIDAD SOCIAL 2022.pdf",
-#             mime="application/pdf"
-#         )
-# #%%
 st.image('img/seremi-100-años.png', width=300)
 st.title("Análisis de Comunas en Región Metropolitana: Indice de prioridad social (IPS)")
 st.write("""
     Bienvenido al dashboard de análisis de comunas en la Región Metropolitana. 
     Aquí puedes encontrar información sobre los indicadores de prioridad social de cada comuna.
 """)
-display_pdf(pdf_url)
 #%%
 # Cargar los datos
 df_ips = pd.read_excel('data_raw/IPS_2022.xlsx')
