d2478de3cdb44a5ae2eb6e3ba38564bc4e843b93
santiagofh <santiago.farias@gmail.com>
2024-06-11 10:53:15 -0400

    Update 02_Ambiental.py

--- pages/02_Ambiental.py
+++ pages/02_Ambiental.py
@@ -37,6 +37,12 @@ st.write('Este tablero interactivo presenta indicadores ambientales priorizados 
 # Emisiones de dióxido de carbono para fuentes puntuales
 st.write('## Emisiones de dióxido de carbono para fuentes puntuales')
 st.write('Las emisiones de dióxido de carbono (CO₂) son un indicador clave para evaluar la contribución de diferentes fuentes a la contaminación del aire y al cambio climático. Este gráfico muestra las emisiones de CO₂ por fuentes puntuales de las regiones.')
+
+st.write('### Emisiones de dióxido de carbono CO2, por tipo de fuente segregadas por región, 2019.')
+infogram_url0 = "https://infogram.com/1pgd26w1w7ekpgs9p9wg021d0ltwl9rrl3q"
+st.components.v1.iframe(infogram_url0, width=800, height=600, scrolling=True)
+st.write('fuente: https://retc.mma.gob.cl/indicadores/emisiones-al-aire/')
+
 st.write('### Emisiones de dióxido de carbono (CO2) desagregadas por región para fuentes puntuales por rubro, 2019')
 infogram_url1 = "https://infogram.com/1prdpmyp12vd2xugvp1n53lre1smzy7prrw"
 st.components.v1.iframe(infogram_url1, width=800, height=600, scrolling=True)
@@ -44,7 +50,7 @@ st.write('fuente: https://retc.mma.gob.cl/indicadores/emisiones-al-aire/')
 
 # Emisiones de material particulado respirable fino para fuentes puntuales
 st.write('## Emisiones de material particulado respirable fino para fuentes puntuales')
-st.write('El material particulado respirable fino (PM2.5) es una de las principales preocupaciones ambientales debido a sus efectos adversos en la salud humana. Este gráfico muestra las emisiones de PM2.5 por fuentes puntuales en la Región Metropolitana.')
+st.write('El material particulado respirable fino es una de las principales preocupaciones ambientales debido a sus efectos adversos en la salud humana.')
 
 st.write('### Emisiones de material particulado respirable (MP10) desagregadas por región para fuentes puntuales por rubro, 2019.')
 infogram_url2 = "https://infogram.com/1pyxvngg60xnvwt3m51k5g1wv5tyw2xll29"
@@ -60,8 +66,7 @@ st.write('fuente: https://retc.mma.gob.cl/indicadores/emisiones-al-aire/')
 import streamlit as st
 
 # Título de la sección
-st.write('## Mapas Open Data')
-st.write('## Resumen de Declaraciones de Establecimientos Industriales y/o Municipales 2019')
+st.write('## Mapa de Declaraciones de Establecimientos Industriales y/o Municipales 2019')
 
 # Descripción de la sección
 st.write("""
@@ -91,14 +96,12 @@ Contiene el resumen de las declaraciones de los Establecimientos industriales y/
 """)
 
 # Incrustar el mapa de OpenStreetMap usando un iframe
-map_url = "https://umap.openstreetmap.fr/es/map/declaracion-jurada-anual-2018-toneladas_517111?scaleControl=true&miniMap=true&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=true&embedControl=null&datalayersControl=null&onLoadPanel=undefined&captionBar=false&fullscreenControl=true#6/-35.693/-72.378"
+map_url = "https://umap.openstreetmap.fr/es/map/declaracion-jurada-anual-2018-toneladas_517111?scaleControl=true&miniMap=true&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=false&searchControl=true&tilelayersControl=true&embedControl=null&datalayersControl=null&onLoadPanel=undefined&captionBar=false&fullscreenControl=true#11/-33.4664/-70.7870"
 st.components.v1.iframe(map_url, width=800, height=600, scrolling=True)
 
 
 st.write('## Incidencia de intoxicaciones agudas por plaguicidas (IAP)')
 st.write('La incidencia de intoxicaciones agudas por plaguicidas es un importante indicador de los riesgos asociados al uso de estos productos químicos en la agricultura y otros sectores.')
-
-
 st.write('## Riesgo de impactos de salud a consecuencias de olas de calor')
 st.write('## Consumo promedio de agua por comuna')
 st.write('## Decretos de Zonas de Escasez Hídrica 2008-2023')
