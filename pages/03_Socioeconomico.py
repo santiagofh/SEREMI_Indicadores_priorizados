fbe3db6d91e0a207b570043d2860f236623bc118
santiagofh <santiago.farias@gmail.com>
2024-06-11 09:17:48 -0400

    Update 03_Socioeconomico.py

--- pages/03_Socioeconomico.py
+++ pages/03_Socioeconomico.py
@@ -97,55 +97,63 @@ for category, table in tables.items():
 
 
 #%%
+
 # Pobreza de ingresos
 st.write(f"### Pobreza de ingresos para {comuna_seleccionada}")
-casen_pobrezai = casen_csv.loc[casen_csv.Category=='POBREZA DE INGRESOS']
+casen_pobrezai = casen_csv.loc[casen_csv.Category == 'POBREZA DE INGRESOS']
 casen_pobrezai_comuna = casen_pobrezai[casen_pobrezai['Comuna'] == filtro_comuna]
 casen_pobrezai_comuna['Pobres'].fillna(casen_pobrezai_comuna['Pobres 2020'], inplace=True)
 casen_pobrezai_comuna['Pobres'] = casen_pobrezai_comuna['Pobres'] / 100
-fig = px.bar(
+fig_pobreza_ingresos = px.bar(
     casen_pobrezai_comuna,
     x='Año',
     y='Pobres',
     title=f"Pobreza de ingresos en {comuna_seleccionada}",
-    labels={'Pobres': 'Porcentaje de Pobreza de Ingresos', 'Año': 'Año'}
+    labels={'Pobres': 'Porcentaje de Pobreza de Ingresos', 'Año': 'Año'},
+    text=casen_pobrezai_comuna['Pobres'].apply(lambda x: '{0:1.2f}%'.format(x*100))
 )
-fig.update_layout(
-    yaxis_tickformat=",.2%"
+fig_pobreza_ingresos.update_layout(
+    yaxis_tickformat=",.2%",
+    yaxis_title='Porcentaje de Pobreza de Ingresos'
 )
-st.plotly_chart(fig)
+fig_pobreza_ingresos.update_traces(texttemplate='%{text}', textposition='outside')
 
-st.write('_Fuente: Elaboración propia a partir de encusta CASEN 2017, 2020 y 2022_')
+st.markdown("<div style='display: flex; justify-content: center;'>", unsafe_allow_html=True)
+st.plotly_chart(fig_pobreza_ingresos, use_container_width=False)
+st.markdown("</div>", unsafe_allow_html=True)
+st.write('_Fuente: Elaboración propia a partir de encuesta CASEN 2017, 2020 y 2022_')
 st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
 
 #%%
 # Pobreza multidimensional
 st.write(f"### Pobreza multidimensional para {comuna_seleccionada}")
-casen_pobrezam = casen_csv.loc[casen_csv.Category=='POBREZA MULTIDIMENSIONAL']
+casen_pobrezam = casen_csv.loc[casen_csv.Category == 'POBREZA MULTIDIMENSIONAL']
 casen_pobrezam_comuna = casen_pobrezam[casen_pobrezam['Comuna'] == filtro_comuna]
 casen_pobrezam_comuna['Pobres'] = casen_pobrezam_comuna['Pobres'] / 100
-fig = px.bar(
+fig_pobreza_multidimensional = px.bar(
     casen_pobrezam_comuna,
     x='Año',
     y='Pobres',
     title=f"Pobreza multidimensional en {comuna_seleccionada}",
-    labels={'Pobres': 'Porcentaje de Pobreza Multidimensional', 'Año': 'Año'}
+    labels={'Pobres': 'Porcentaje de Pobreza Multidimensional', 'Año': 'Año'},
+    text=casen_pobrezam_comuna['Pobres'].apply(lambda x: '{0:1.2f}%'.format(x*100))
 )
-fig.update_layout(
-    yaxis_tickformat=",.2%"
+fig_pobreza_multidimensional.update_layout(
+    yaxis_tickformat=",.2%",
+    yaxis_title='Porcentaje de Pobreza Multidimensional'
 )
-st.plotly_chart(fig)
-st.write('_Fuente: Elaboración propia a partir de encusta CASEN 2017, 2020 y 2022_')
-st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
+fig_pobreza_multidimensional.update_traces(texttemplate='%{text}', textposition='outside')
 
+st.markdown("<div style='display: flex; justify-content: center;'>", unsafe_allow_html=True)
+st.plotly_chart(fig_pobreza_multidimensional, use_container_width=False)
+st.markdown("</div>", unsafe_allow_html=True)
+st.write('_Fuente: Elaboración propia a partir de encuesta CASEN 2017, 2020 y 2022_')
+st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
 #%%
-## Ingresos
+# Ingresos
 st.write(f"### Ingresos en {comuna_seleccionada}")
 casen_ingresos = casen_csv[casen_csv['Category'] == 'INGRESOS']
 casen_ingresos_comuna = casen_ingresos[casen_ingresos['Comuna'] == filtro_comuna]
-
-# Reemplazar 'No reporta' por NaN
-# casen_ingresos_comuna.replace('No reporta', np.nan, inplace=True)
 
 casen_ingresos_comuna_long = pd.melt(
     casen_ingresos_comuna,
@@ -155,10 +163,9 @@ casen_ingresos_comuna_long = pd.melt(
     value_name='Monto'
 )
 
-# Asegurarse de que 'Año' sea categórico
 casen_ingresos_comuna_long['Año'] = casen_ingresos_comuna_long['Año'].astype(str)
 
-fig = px.bar(
+fig_ingresos = px.bar(
     casen_ingresos_comuna_long,
     x='Año',
     y='Monto',
@@ -167,116 +174,48 @@ fig = px.bar(
     title=f"Distribución de Ingresos en {comuna_seleccionada}",
     labels={'Monto': 'Monto de Ingreso', 'Año': 'Año'}
 )
-fig.update_layout(
+fig_ingresos.update_layout(
     yaxis_tickformat=".0f"
 )
-st.plotly_chart(fig)
-st.write('_Fuente: Elaboración propia a partir de encusta CASEN 2017, 2020 y 2022_')
-st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
 
+st.markdown("<div style='display: flex; justify-content: center;'>", unsafe_allow_html=True)
+st.plotly_chart(fig_ingresos, use_container_width=False)
+st.markdown("</div>", unsafe_allow_html=True)
+st.write('_Fuente: Elaboración propia a partir de encuesta CASEN 2017, 2020 y 2022_')
+st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
 
 #%%
 # Participación laboral
 st.write(f"### Participación laboral en {comuna_seleccionada}")
-casen_tasas_participacion = casen_csv.loc[casen_csv.Category=='TASAS PARTICIPACIÓN LABORAL']
+casen_tasas_participacion = casen_csv.loc[casen_csv.Category == 'TASAS PARTICIPACIÓN LABORAL']
 casen_tasas_participacion_comuna = casen_tasas_participacion[casen_tasas_participacion['Comuna'] == filtro_comuna]
 casen_tasas_participacion_comuna['Hombres'] = casen_tasas_participacion_comuna['Hombres'] / 100
 casen_tasas_participacion_comuna['Mujeres'] = casen_tasas_participacion_comuna['Mujeres'] / 100
-casen_tasas_participacion_comuna_long = pd.melt(    
+casen_tasas_participacion_comuna_long = pd.melt(
     casen_tasas_participacion_comuna,
     id_vars=['Año'],
     value_vars=['Hombres', 'Mujeres'],
     var_name='Sexo',
     value_name='Porcentaje'
 )
-fig = px.bar(
+fig_participacion_laboral = px.bar(
     casen_tasas_participacion_comuna_long,
     x='Año',
     y='Porcentaje',
     color='Sexo',
     barmode='group',
     title=f"Tasas de participación laboral en {comuna_seleccionada}",
-    labels={'Porcentaje': 'Porcentaje de Participación', 'Año': 'Año'}
+    labels={'Porcentaje': 'Porcentaje de Participación', 'Año': 'Año'},
+    text=casen_tasas_participacion_comuna_long['Porcentaje'].apply(lambda x: '{0:1.2f}%'.format(x*100))
 )
-fig.update_layout(
+fig_participacion_laboral.update_layout(
     yaxis_tickformat=".2%",
     yaxis_title='Porcentaje de Participación'
 )
-st.plotly_chart(fig)
-st.write('_Fuente: Elaboración propia a partir de encusta CASEN 2017, 2020 y 2022_')
-st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
+fig_participacion_laboral.update_traces(texttemplate='%{text}', textposition='outside')
 
-#%%
-# Migrantes
-st.write(f"### Migrantes en {comuna_seleccionada}")
-casen_migrantes = casen_csv.loc[casen_csv.Category=='MIGRANTES']
-casen_migrantes_comuna = casen_migrantes[casen_migrantes['Comuna'] == filtro_comuna]
-casen_migrantes_comuna['Población nacida en Chile'] = casen_migrantes_comuna['Población nacida en Chile'] / 100
-casen_migrantes_comuna['Población nacida fuera de Chile'] = casen_migrantes_comuna['Población nacida fuera de Chile'] / 100
-casen_migrantes_comuna['No sabe'] = casen_migrantes_comuna['No sabe'] / 100
-casen_migrantes_comuna_long = pd.melt(
-    casen_migrantes_comuna,
-    id_vars=['Año'],
-    value_vars=['Población nacida en Chile', 'Población nacida fuera de Chile', 'No sabe'],
-    var_name='Tipo de Población',
-    value_name='Porcentaje'
-)
-fig_bar = px.bar(
-    casen_migrantes_comuna_long,
-    x='Año',
-    y='Porcentaje',
-    color='Tipo de Población',
-    barmode='group',
-    title=f"Distribución de la Población Migrante en {comuna_seleccionada}",
-    labels={'Porcentaje': 'Porcentaje de la Población', 'Año': 'Año'}
-)
-fig_trend = px.scatter(
-    casen_migrantes_comuna_long[casen_migrantes_comuna_long['Tipo de Población'] == 'Población nacida fuera de Chile'],
-    x='Año',
-    y='Porcentaje',
-    trendline="ols",
-    labels={'Porcentaje': 'Porcentaje de la Población', 'Año': 'Año'}
-)
-fig_trend.data[1].marker.color = 'red'
-fig_trend.data[1].name = 'Tendencia de Migrantes'
-fig_trend.data[0].showlegend = False
-fig = fig_bar.add_traces(fig_trend.data[1:])
-fig.update_layout(
-    yaxis_tickformat=".2%",
-    yaxis_title='Porcentaje de la Población'
-)
-st.plotly_chart(fig)
-st.write('_Fuente: Elaboración propia a partir de encusta CASEN 2017, 2020 y 2022_')
-st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
-
-#%%
-# Pertenencia Étnica
-st.write(f"### Pertenencia Étnica en {comuna_seleccionada}")
-casen_etnias = casen_csv.loc[casen_csv.Category=='ETNIAS']
-casen_etnias_comuna = casen_etnias[casen_etnias['Comuna'] == filtro_comuna]
-casen_etnias_comuna['Pertenece a algún pueblo originario']=casen_etnias_comuna['Pertenece a algún pueblo originario']/100
-casen_etnias_comuna['No pertenece a ningún pueblo originario']=casen_etnias_comuna['No pertenece a ningún pueblo originario']/100
-
-casen_etnias_comuna_long = pd.melt(
-    casen_etnias_comuna,
-    id_vars=['Año'],
-    value_vars=['Pertenece a algún pueblo originario', 'No pertenece a ningún pueblo originario'],
-    var_name='Tipo de Población',
-    value_name='Porcentaje'
-)
-fig = px.bar(
-    casen_etnias_comuna_long,
-    x='Año',
-    y='Porcentaje',
-    color='Tipo de Población',
-    barmode='group',
-    title=f"Distribución de la población por pertenencia a pueblo originario en {comuna_seleccionada}",
-    labels={'Porcentaje': 'Porcentaje de la Población', 'Año': 'Año'}
-)
-fig.update_layout(
-    yaxis_tickformat=".2%",
-    yaxis_title='Porcentaje de la Población'
-)
-st.plotly_chart(fig)
-st.write('_Fuente: Elaboración propia a partir de encusta CASEN 2017, 2020 y 2022_')
+st.markdown("<div style='display: flex; justify-content: center;'>", unsafe_allow_html=True)
+st.plotly_chart(fig_participacion_laboral, use_container_width=False)
+st.markdown("</div>", unsafe_allow_html=True)
+st.write('_Fuente: Elaboración propia a partir de encuesta CASEN 2017, 2020 y 2022_')
 st.write('_https://observatorio.ministeriodesarrollosocial.gob.cl/encuesta-casen_')
