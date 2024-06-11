2b3862b5b6d36ca38a8ae1a2a525c3c38b82b94c
santiagofh <santiago.farias@gmail.com>
2024-06-11 11:36:58 -0400

    Update 04_Indice_Prioridad_Social.py

--- pages/04_Indice_Prioridad_Social.py
+++ pages/04_Indice_Prioridad_Social.py
@@ -1,27 +1,25 @@
 import streamlit as st
 import pdfplumber
-import requests
-from io import BytesIO
 
 # Configuración de la página
 st.set_page_config(page_title="Análisis de Comunas en Región Metropolitana", layout='wide', initial_sidebar_state='expanded')
 
-# Sidebar
-st.sidebar.write("## Índice de prioridad social")
-
 # URL del PDF
 pdf_url = "data_raw/ÍNDICE DE PRIORIDAD SOCIAL 2022.pdf"
 
-# Función para mostrar el PDF
-def show_pdf(file_url):
-    response = requests.get(file_url)
-    file_bytes = BytesIO(response.content)
-    with pdfplumber.open(file_bytes) as pdf:
-        num_pages = len(pdf.pages)
-        st.write(f"Total de páginas: {num_pages}")
-        for i in range(num_pages):
-            page = pdf.pages[i]
-            st.image(page.to_image().original, caption=f'Página {i + 1}', use_column_width=True)
+def display_pdf(file_path):
+    with open(file_path, "rb") as file:
+        st.download_button(
+            label="Descargar Índice de Prioridad Social 2022",
+            data=file,
+            file_name="ÍNDICE DE PRIORIDAD SOCIAL 2022.pdf",
+            mime="application/pdf"
+        )
 
-# Mostrar el PDF desde la URL
-show_pdf(pdf_url)
+
+st.title("Análisis de Comunas en Región Metropolitana")
+st.write("""
+    Bienvenido al dashboard de análisis de comunas en la Región Metropolitana. 
+    Aquí puedes encontrar información sobre los indicadores de prioridad social de cada comuna.
+""")
+display_pdf(pdf_url)
