584c2918bf9ca5b20617f72f5e36171c5d9b54d3
santiagofh <santiago.farias@gmail.com>
2024-06-11 11:25:35 -0400

    a

--- pages/04_Indice_Prioridad_Social.py
+++ pages/04_Indice_Prioridad_Social.py
@@ -1,4 +1,3 @@
-#%%
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -10,11 +9,23 @@ import json
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import numpy as np
+import pdfplumber
+from PIL import Image
 
-#%%
-# INICIO DE LA PAGINA
+# Configuración de la página
 st.set_page_config(page_title="Análisis de Comunas en Región Metropolitana", layout='wide', initial_sidebar_state='expanded')
 
-#%%
 # Sidebar
-st.write("## Indice de prioridad social")
+st.sidebar.write("## Índice de prioridad social")
+
+# URL del PDF (solo para referencia)
+pdf_url = "https://github.com/santiagofh/SEREMI_Indicadores_priorizados/blob/main/data_raw/%C3%8DNDICE%20DE%20PRIORIDAD%20SOCIAL%202022.pdf"
+
+# Función para mostrar el PDF
+def show_pdf(file_path):
+    with pdfplumber.open(file_path) as pdf:
+        num_pages = len(pdf.pages)
+        st.write(f"Total de páginas: {num_pages}")
+        for i in range(num_pages):
+            page = pdf.pages[i]
+            st.image(page.to_image().original, caption=f'Página {i + 1}', use_column_width=True)
