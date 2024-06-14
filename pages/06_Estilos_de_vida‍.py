352ce79386ecbd0e8d5dfb8037b75a5fbfb3c6d6
santiagofh <santiago.farias@gmail.com>
2024-06-14 14:30:06 -0400

    img

--- pages/06_Estilos_de_vida‍.py
+++ pages/06_Estilos_de_vida‍.py
@@ -8,3 +8,6 @@ st.set_page_config(page_title="Análisis de Comunas en Región Metropolitana", l
 # TITULO INTRODUCCION
 st.image('img/seremi-100-años.png', width=300)
 st.write('# Región Metropolitana y sus comunas: Estilo de vida')
+
+st.write('Pagina en construcción')
+st.image('img/Personajes-Mujer obrera 2.png')
